472cb53f5d648ca858d7b5b9ad911be2d8ca07c4
jhonAQ-gif <jhonathan1alvarez@gmail.com>
2026-04-20 20:31:15 -0300

    Add comments and examples for data types in Python

---
+++ Tipos_de_datos.py
@@ -0,0 +1,16 @@
+#tipos de datos texto
+
+"soy sigma w"
+
+"""tus datos son:
+nombre: jhon
+edad:13"""
+#(tambien se puede con :[' '])
+
+#tipos de dato numeros 
+int(54)
+float(65.7)
+
+#tipos de dato boleanos
+True
+False
