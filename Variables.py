b7b4b1361a60aa4ee61d9b83349c883542ed3ce7
jhonAQ-gif <jhonathan1alvarez@gmail.com>
2026-04-27 14:41:13 -0300

    Add variable definitions and string operations

---
+++ Variables.py
@@ -0,0 +1,20 @@
+#Definiendo una variable
+
+nombre = "Lucas"
+
+#definiendo variable con snake_case
+
+nombre_completo = "lucas torrez"
+#concatenar con +
+
+bienvenida = "Hola" + "¿Como estas?"
+
+#concatenar con f-strings
+
+bienvenida = f"Hola {nombre_completo} ¿Como estas?"
+
+#operadores de pertenencia (in / not in)
+
+print("Lucas" in bienvenida) #true
+
+print ("Lucas" not in bienvenida)#false
